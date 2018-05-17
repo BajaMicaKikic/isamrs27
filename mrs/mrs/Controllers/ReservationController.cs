33620309050d3cf8b32fbf22470050c1a13ee56a
Milica Markovic <milicam.2194@gmail.com>
2018-05-17 14:17:21 +0200

    Added view for reservations.

--- mrs/mrs/Controllers/ReservationController.cs
+++ mrs/mrs/Controllers/ReservationController.cs
@@ -3,13 +3,29 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using mrs.ApplicationCore.Entities;
+using mrs.Infrastructure.Data;
 
 namespace mrs.Controllers
 {
     public class ReservationController : Controller
     {
-        public IActionResult Index()
+        public ActionResult Index()
         {
+            return View();
+        }
+
+        //public AcceptedResult Reservate() {
+
+        //}
+
+        [HttpPost]
+        public ActionResult Reservate(CultureObject co, Projection po, CultureObjectHall coh, Screening sc)
+        {
+            if (ModelState.IsValid)
+            {
+                MrsContext mrs = new MrsContext();
+            }
             return View();
         }
     }
