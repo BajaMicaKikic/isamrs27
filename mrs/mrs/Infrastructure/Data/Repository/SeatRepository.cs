d2a3f3f151bccaa1c3054fc5aa4b28819f4326dd
Milica Markovic <milicam.2194@gmail.com>
2018-06-14 14:30:20 +0200

    Code refactoring no2

--- mrs/mrs/Infrastructure/Data/Repository/SeatRepository.cs
+++ mrs/mrs/Infrastructure/Data/Repository/SeatRepository.cs
@@ -4,6 +4,11 @@
     using System.Threading.Tasks;
     using mrs.ApplicationCore.Entities;
     using mrs.ApplicationCore.Interfaces.Repository;
+    using System.Linq;
+    using System;
+    using mrs.ViewModels;
+    using mrs.ViewModels.Reservation;
+
     /// <summary>
     /// Class for Seat Repository pattern. 
     /// </summary>
@@ -21,10 +26,29 @@
 
         public async Task<List<Seat>> GetSeatByHallSegmentIdAsync(long HallSegmentId)
         {
-            List<Seat> seats = await ListAllAsync();
-            var seatList = seats.FindAll(s => s.HallSegmentId== HallSegmentId);
+            MrsContext mrs = new MrsContext();
+            List<Seat> seats = await ListAllAsync();           
+            List<SeatReservation> seatReservations = mrs.SeatReservations.ToList();
+            /* Upit za slobodna mesta
+             SELECT seats.SeatId,seats.SeatNumber FROM seats LEFT JOIN seatreservation on seats.SeatId=seatreservation.SeatId 
+             where (seatreservation.IsActive=0 AND seatreservation.IsReserved=0)  or seatreservation.SeatId is null 
+             */
+            // var xx = seatReservations.FindAll(s => s.IsActive == false && s.IsReserved == false);
+            var query = (from aa in seats
+                         join bb in seatReservations.Where(s => s.IsReserved == true && s.IsActive == true)
+                         on aa.Id equals bb.SeatId into cc
+                         from x in cc.DefaultIfEmpty()
+                         where x == null
+                         select new Seat
+                         {
+                             Id=(int)aa.Id,
+                             SeatNumber = aa.SeatNumber,
+                             HallSegmentId=(int) aa.HallSegmentId
+                         }).ToList();
 
-            return seatList;
+            var seatList = query.Where(s => s.HallSegmentId == HallSegmentId);
+
+            return seatList.ToList();
         }
     }
 }
